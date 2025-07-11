85d4301d94a256af9fbbc32eed98cc83fcdde4a0
ethanzywang <ethanzywang@tencent.com>
2025-07-11 16:36:48 +0800

    更新user建表sql

--- src/main/resources/database/user.sql
+++ src/main/resources/database/user.sql
@@ -1,7 +1,7 @@
-create table peanut_user_center.user
+create table if not exists peanut_user_center.user
 (
     id           bigint auto_increment comment 'id'
-        primary key,
+    primary key,
     username     varchar(256)             null comment '用户昵称',
     userAccount  varchar(256)             null comment '账号',
     avatarUrl    varchar(1024)            null comment '用户头像',
@@ -11,8 +11,10 @@ create table peanut_user_center.user
     email        varchar(512)             null comment '邮箱',
     userStatus   int      default 0       not null comment '用户状态  0-正常',
     createTime   datetime default (now()) null comment '创建时间',
-    updateTime   datetime default (now()) null on update now() comment '更新时间',
-    idDelete     int      default 0       not null comment '是否删除'
-)
+    updateTime   datetime default (now()) null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     int      default 0       not null comment '是否删除',
+    role         int      default 0       not null comment '用户角色 0-普通用户 1-管理员',
+    code         varchar(128)             null comment '用户编号'
+    )
     comment '用户';
 
